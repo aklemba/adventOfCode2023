07418a39931a4bf4caf8c3c44a99fb7434f12565
Adam Klemba <adam.a.klemba@gmail.com>
2023-12-07 22:07:33 +0100

    Day 1 part 2

--- src/main/kotlin/pl/klemba/aoc/day1/Main.kt
+++ src/main/kotlin/pl/klemba/aoc/day1/Main.kt
@@ -3,11 +3,39 @@ package pl.klemba.aoc.day1
 import java.io.File
 
 fun main() {
-  File("src/main/kotlin/pl/klemba/aoc/day1/input.txt")
+  // ----- PART 1 -----
+  File(inputPath)
     .readLines()
     .map { extractCalibrationValue(it) }
     .reduce { accumulator, calibrationValue -> accumulator + calibrationValue }
     .let { println(it) }
+
+  // ----- PART 2 -----
+  File(inputPath)
+    .readLines()
+    .map { rawLine -> formatToDigits(rawLine) }
+    .map { extractCalibrationValue(it) }
+    .reduce { accumulator, calibrationValue -> accumulator + calibrationValue }
+    .let { println(it) }
+}
+
+fun formatToDigits(rawLine: String): String {
+  var calibrationValue = ""
+
+  var rawLineLeftToCheck = rawLine
+  while (rawLineLeftToCheck.isNotEmpty()) {
+    rawLineLeftToCheck.getDigitAtStart()?.let { calibrationValue += it }
+    rawLineLeftToCheck = rawLineLeftToCheck.substring(1)
+  }
+  return calibrationValue
+}
+
+private fun String.getDigitAtStart(): Int? {
+  println("length: " + this.length)
+  if (first().isDigit()) return first().digitToInt()
+  digitNames
+    .forEach { mapEntry -> if (startsWith(mapEntry.key)) return mapEntry.value }
+  return null
 }
 
 fun extractCalibrationValue(line: String): Int {
@@ -15,3 +43,25 @@ fun extractCalibrationValue(line: String): Int {
   val lastDigit = line.last { it.isDigit() }
   return "$firstDigit$lastDigit".toInt()
 }
+
+fun transformDigitNames(line: String) =
+  digitNames.keys
+    .fold(line) { accumulator, digitName -> accumulator.transformDigitName(digitName) }
+
+private fun String.transformDigitName(digitName: String): String =
+  this.replace(digitName, digitNames[digitName].toString())
+
+private const val inputPath = "src/main/kotlin/pl/klemba/aoc/day1/input.txt"
+
+private val digitNames: Map<String, Int> = listOf(
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine"
+).mapIndexed { index, digitName -> digitName to index + 1 }
+  .toMap()
