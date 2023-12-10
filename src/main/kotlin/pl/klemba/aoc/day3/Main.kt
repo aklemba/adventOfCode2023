daa386f8c1a537d44acf869738903bf86a252a10
Adam Klemba <adam.a.klemba@gmail.com>
2023-12-10 21:21:42 +0100

    Day3 part 2

--- src/main/kotlin/pl/klemba/aoc/day3/Main.kt
+++ src/main/kotlin/pl/klemba/aoc/day3/Main.kt
@@ -16,8 +16,50 @@ fun main() {
         .let { println(it) }
     }
 
+  // ----- PART 2 -----
+  val schematic = File(inputPath)
+    .readLines()
+
+  val numbersSchematic = schematic.mapIndexed { lineIndex, line ->
+    numberRegex.findAll(line)
+      .filter { schematic.checkIfAdjacentToASymbol(it.range, lineIndex) }
+      .toList()
+  }
+
+  schematic.mapIndexed { lineIndex, line ->
+    gearRegex.findAll(line)
+      .map {
+        numbersSchematic.sumGearRatio(it.range, lineIndex)
+      }
+      .sum()
+  }.sum()
+    .let { println(it) }
+
 
 }
+
+private fun List<List<MatchResult>>.sumGearRatio(range: IntRange, lineIndex: Int): Int {
+  val listOfAdjacentNumbers = mutableListOf<MatchResult>()
+  if (lineIndex != 0) {
+    listOfAdjacentNumbers.addAll(this[lineIndex - 1].getNumbersForIndex(range, this[0].lastIndex))
+  }
+  if (lineIndex != this.lastIndex) {
+    listOfAdjacentNumbers.addAll(this[lineIndex + 1].getNumbersForIndex(range, this[0].lastIndex))
+  }
+
+  listOfAdjacentNumbers.addAll(this[lineIndex].getNumbersForIndex(range, this[0].lastIndex))
+  return if (listOfAdjacentNumbers.size == 2) {
+    listOfAdjacentNumbers.map { it.value.toInt() }.reduce { acc, value -> acc * value }
+  }
+  else 0
+}
+
+private fun List<MatchResult>.getNumbersForIndex(range: IntRange, lastIndex: Int): List<MatchResult> {
+  val rangeToCheck = getRangeToCheck(range, lastIndex)
+  return this.filter { it.isAdjacentToGear(rangeToCheck) }
+}
+
+private fun MatchResult.isAdjacentToGear(rangeToCheck: IntRange) = range.intersect(rangeToCheck).isNotEmpty()
 
 private fun List<String>.checkIfAdjacentToASymbol(numberIndices: IntRange, lineIndex: Int): Boolean {
   // check top and bottom
@@ -30,22 +72,27 @@ private fun List<String>.checkIfAdjacentToASymbol(numberIndices: IntRange, lineI
 
   // check sides
   if (numberIndices.first != 0) {
-    if(this[lineIndex][numberIndices.first - 1].isASymbol()) return true
+    if (this[lineIndex][numberIndices.first - 1].isASymbol()) return true
   }
   if (numberIndices.last != this[lineIndex].lastIndex) {
-    if(this[lineIndex][numberIndices.last + 1].isASymbol()) return true
+    if (this[lineIndex][numberIndices.last + 1].isASymbol()) return true
   }
 
   return false
 }
 
 private fun String.checkIfSymbolsExistForRange(indexRange: IntRange, lineLastIndex: Int): Boolean {
+  val rangeToCheck: IntRange = getRangeToCheck(indexRange, lineLastIndex)
+  return substring(rangeToCheck).any { it.isASymbol() }
+}
+
+private fun getRangeToCheck(indexRange: IntRange, lineLastIndex: Int): IntRange {
   val rangeToCheck: IntRange = when {
     indexRange.first == 0 -> IntRange(0, indexRange.last + 1)
-    indexRange.last == lineLastIndex -> IntRange(indexRange.first -1 , indexRange.last)
+    indexRange.last == lineLastIndex -> IntRange(indexRange.first - 1, indexRange.last)
     else -> IntRange(indexRange.first - 1, indexRange.last + 1)
   }
-  return substring(rangeToCheck).any { it.isASymbol() }
+  return rangeToCheck
 }
 
 private fun Char.isASymbol() = this.isDigit().not() && this != '.'
@@ -53,3 +100,4 @@ private fun Char.isASymbol() = this.isDigit().not() && this != '.'
 private const val inputPath = "src/main/kotlin/pl/klemba/aoc/day3/input.txt"
 
 private val numberRegex = Regex("\\d+")
+private val gearRegex = Regex("\\*")
