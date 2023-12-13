22b51c688c872214717b9b5100ea3fc82aa81f6d
Adam Klemba <adam.a.klemba@gmail.com>
2023-12-13 17:36:06 +0100

    Day 4 Part 2

--- src/main/kotlin/pl/klemba/aoc/day4/Main.kt
+++ src/main/kotlin/pl/klemba/aoc/day4/Main.kt
@@ -9,19 +9,34 @@ fun main() {
     .readLines()
     .sumOf { card -> getNumberOfPointsForCard(card) }
     .let { println(it) }
+
+  // ----- PART 2 -----
+  val cardList = File(inputPath)
+    .readLines()
+
+  val amountOfScratchCards = IntArray(cardList.size) { 1 }
+
+  cardList
+    .foldIndexed(amountOfScratchCards) { index, acc, card -> calculateNumberOfCards(card, acc, index) }
+    .sum()
+    .let { println(it) }
 }
 
 fun getNumberOfPointsForCard(card: String): Int {
+  val amountOfMatchingNumbers = getNumberOfMatchingNumbers(card)
+  return if (amountOfMatchingNumbers == 1) 1 else 2.0.pow(amountOfMatchingNumbers - 1).toInt()
+}
+
+fun getNumberOfMatchingNumbers(card: String): Int {
   val winningNumbers = getWinningNumbers(card)
   val numbersYouHave = getNumbersYouHave(card)
-  val amountOfMatchingNumbers = winningNumbers.intersect(numbersYouHave.toSet()).size
-  return if(amountOfMatchingNumbers == 1) 1 else 2.0.pow(amountOfMatchingNumbers - 1).toInt()
+  return winningNumbers.intersect(numbersYouHave.toSet()).size
 }
 
 fun getWinningNumbers(card: String): List<Int> =
   winningNumbersRegex.find(card)
     ?.value
-    ?.removeRange(0,2)
+    ?.removeRange(0, 2)
     ?.trim()
     ?.split("  ", " ")
     ?.map { it.toInt() }
@@ -30,11 +45,20 @@ fun getWinningNumbers(card: String): List<Int> =
 fun getNumbersYouHave(card: String): List<Int> =
   numbersYouHaveRegex.find(card)
     ?.value
-    ?.removeRange(0,2)
+    ?.removeRange(0, 2)
     ?.trim()
     ?.split("  ", " ")
     ?.map { it.toInt() }
     ?: throw IllegalStateException("No your numbers found!")
+
+fun calculateNumberOfCards(card: String, acc: IntArray, index: Int): IntArray {
+  val numberOfPoints = getNumberOfMatchingNumbers(card)
+  val numberOfCards = acc[index]
+  repeat(numberOfPoints) { i ->
+    acc[index + i + 1] += numberOfCards
+  }
+  return acc
+}
 
 
 private val winningNumbersRegex = Regex(""": +\d+(?: +\d+)+""")
